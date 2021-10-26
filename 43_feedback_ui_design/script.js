84b41ca9b0ef1e8d21b25e189fc18334cd1c4f00
Kenneth Horn <kennethhorn@Kenneths-MacBook-Air.local>
2021-10-25 22:33:11 -0700

    new project feedback ui

---
+++ 43_feedback_ui_design/script.js
@@ -0,0 +1,40 @@
+const ratings = document.querySelectorAll('.rating')
+const ratingsCtn = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+
+let selectedRating = 'Satisfied'
+
+ratingsCtn.addEventListener('click', (e) => {
+  if (e.target.parentNode.classList.contains('rating') ) {
+    removeActive()
+    e.target.parentNode.classList.add('active')
+    if (e.target.nextElementSibling) {
+      selectedRating = e.target.nextElementSibling.innerHTML
+    } else {
+      selectedRating = e.target.innerHTML
+    }
+  }
+  if (e.target.classList.contains('rating') ) {
+    removeActive()
+    e.target.classList.add('active')
+    selectedRating = e.target.lastElementChild.innerHTML
+  }
+  console.log(selectedRating)
+})
+
+sendBtn.addEventListener('click', (e) => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>Your feedback helps us improve our customer support.</p>
+  `
+})
+
+function removeActive() {
+  for(let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove('active')
+  }
+}
